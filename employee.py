f12aaca83e8d3747f7d40d022e7c3d76058d0291
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-09-24 20:07:10 +0100

    Solution 5

--- employee.py
+++ employee.py
@@ -33,22 +33,22 @@ ariel = Employee('Ariel')
 def get_pay(Employee):
     pay = 0
     match Employee.name:
-        case "billie":
+        case "Billie":
             pay = 4000
             print("Billie works on a monthly salary of 4000.  Their total pay is 4000.")
-        case "charlie":
+        case "Charlie":
             pay = 100 * 25
             print("Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.")
-        case "renee":
+        case "Renee":
             pay = 3000 + 4(200)
             print("Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.")
-        case "jan":
+        case "Jan":
             pay = (150*25) + (3*220)
             print("Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.")
-        case "robbie":
+        case "Robbie":
             pay = 2000 + 1500
             print("Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.")
-        case "ariel":
+        case "Ariel":
             pay = (120*30) + 600
             print("Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.")
 
